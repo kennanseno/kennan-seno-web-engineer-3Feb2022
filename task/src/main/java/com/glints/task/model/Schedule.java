fca12c2b6bcaae76aad6c5c9ebba009c28322ab1
Kennan Seno <seno.kennan@gmail.com>
2022-02-05 00:13:31 +0800

    Update Schedule model
    - Add more getters/setters
    - Add default value on day properties
    - Add methods to convert raw day data

--- task/src/main/java/com/glints/task/model/Schedule.java
+++ task/src/main/java/com/glints/task/model/Schedule.java
@@ -1,6 +1,10 @@
 package com.glints.task.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -9,29 +13,83 @@ import javax.persistence.ManyToOne;
 public class Schedule {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String openingTime;
 
     private String closingTime;
 
-    private Boolean monday;
+    private Boolean monday = false;
 
-    private Boolean tuesday;
+    private Boolean tuesday = false;
 
-    private Boolean wednesday;
+    private Boolean wednesday = false;
 
-    private Boolean thursday;
+    private Boolean thursday = false;
 
-    private Boolean friday;
+    private Boolean friday = false;
 
-    private Boolean saturday;
+    private Boolean saturday = false;
 
-    private Boolean sunday;
+    private Boolean sunday = false;
 
     @ManyToOne
     @JoinColumn(name="restaurant_id", nullable=false)
     private Restaurant restaurant;
+
+    public Schedule() { }
+
+    public void setSchedule(String startingDay, String endDay) {
+        List<String> dayOfTheWeek = List.of("MON", "TUES", "WEDS", "THURS", "FRI", "SAT", "SUN");
+        startingDay = startingDay.toUpperCase();
+        endDay = endDay.toUpperCase();
+
+        // TODO: Need cleaner way to do this. Replace ASAP
+        if(endDay.equals("THU")) {
+            endDay = "THURS";
+        }
+        if(startingDay.equals("THU")) {
+            startingDay = "THURS";
+        }
+        if(endDay.equals("WED")) {
+            endDay = "WEDS";
+        }
+        if(startingDay.equals("WED")) {
+            startingDay = "WEDS";
+        }
+
+        for (Integer startingDayIndex = dayOfTheWeek.indexOf(startingDay); startingDayIndex <= dayOfTheWeek.indexOf(endDay); startingDayIndex++) {
+            setSchedule(dayOfTheWeek.get(startingDayIndex));
+        }
+    }
+
+    public void setSchedule(String day) {
+        String dayOfTheWeek = day.toUpperCase();
+        switch(dayOfTheWeek) {
+            case "MON":
+                monday = true;
+                break;
+            case "TUES":
+                tuesday = true;
+                break;
+            case "WEDS":
+                wednesday = true;
+                break;
+            case "THURS":
+                thursday = true;
+                break;
+            case "FRI":
+                friday = true;
+                break;
+            case "SAT":
+                saturday = true;
+                break;
+            case "SUN":
+                sunday = true;
+                break;
+        }
+    }
 
     public Integer getId() {
         return id;
@@ -71,5 +129,17 @@ public class Schedule {
 
     public String getClosingTime() {
         return closingTime;
+    }
+
+    public void setOpeningTime(String openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    public void setClosingTime(String closingTime) {
+        this.closingTime = closingTime;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
     }
 }
