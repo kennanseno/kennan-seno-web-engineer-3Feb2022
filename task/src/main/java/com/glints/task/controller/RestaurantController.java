635588382c129d5089d0e5eed0d0dfd6553dbf24
Kennan Seno <seno.kennan@gmail.com>
2022-02-05 00:16:59 +0800

    Remove piggybacked code in controller
    Removed code blocks used for transforming raw data

--- task/src/main/java/com/glints/task/controller/RestaurantController.java
+++ task/src/main/java/com/glints/task/controller/RestaurantController.java
@@ -1,11 +1,8 @@
 package com.glints.task.controller;
 
-import java.io.FileNotFoundException;
 import java.util.List;
 import com.glints.task.dao.RestaurantRepository;
-import com.glints.task.dao.ScheduleRepository;
 import com.glints.task.model.Restaurant;
-import com.glints.task.util.DataTransformer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,20 +17,8 @@ public class RestaurantController {
 	@Autowired
 	private RestaurantRepository restaurantRepository;
 
-	@Autowired
-	private ScheduleRepository scheduleRepository;
-
 	@GetMapping
 	public @ResponseBody List<Restaurant> getAllRestaurants() {
-
-		DataTransformer dataTransformer = new DataTransformer("src/main/resources/raw-data.csv");
-		
-		try {
-			dataTransformer.transform(restaurantRepository, scheduleRepository);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
 		return (List<Restaurant>) restaurantRepository.findAll();
 	}
 }
